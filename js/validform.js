0bb90252d82a131b68614547af3afeca18b14397
Katykaze <kathezambrano9@gmail.com>
2022-10-18 17:47:44 +0200

    correciones katy

--- js/validform.js
+++ js/validform.js
@@ -1,10 +1,21 @@
+window.onload = inicio;
+function inicio(){
+    document.formulario.onsubmit = validForm;
+}
+function validForm(){
+    let enviar = true;
+    //----lamando funciones de abajo y si se cumplen todas, enviar el formulario = true
+    return enviar;
+}
+
 //aqui los ejercicios donde hay que usar regexp y llamar a las funciones que hay en validaciones
 
 //razon social
-function validarRazon(razon) {
+function validarRazon() {
+    let razonF = document.formulario.nombre.value;
+    let razon = razonF.trim();
     let cadena_errores = "";
     let enmedio = razon.substr(1, razon.length - 2);
-    //let razon = document.formulario.nombre.value.toUpperCase();
     let regexp = /[ºª\-\.]/;
     let punto = ".";
     if (!esLetra(razon.charAt(0))) {
@@ -24,7 +35,7 @@ function validarRazon(razon) {
 //codigo empleado
 function validarCodEmp(codigo_empresa) {
     let cadena_errores = "";
-    //let codigo_empresa = document.formulario.codempresa.value;
+    let codigo_empresa = document.formulario.codempresa.value;
     let codigo_emp = codigo_empresa.trim();
     let expReg = /^[a-záéíóúüñ0-9]{5,10}$/ig;
     if (!expReg.test(codigo_emp)) {
