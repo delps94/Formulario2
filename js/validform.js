a109598585cf0ce3810c883ec3a228af96b3a01e
delps94 <113589670+delps94@users.noreply.github.com>
2022-10-24 16:54:34 +0200

    Update validform.js
    validaciones de localidad, direccion y codigo postal (Dani)

--- js/validform.js
+++ js/validform.js
@@ -100,3 +100,37 @@ function validarDesplegable() {
 
 }
 //----------------------fin parte Katy------------------------------------------------
+//validar direccion
+function validar_direccion() {
+    let valido = true; 
+    let dir= document.formulario.direccion.value.trim(); 
+    let expregdir = /^[a-z](\w|ª|º|\/|\.|\-)+\w$/i;
+    if (!expregdireccion.test(dir)) {
+        document.formulario.error_direccion.value = "Error, la direccion está mal escrita"; 
+        document.formulario.error_direccion.style = "visibility: visible";
+    }
+    return valido;
+}
+//validar localidad
+function validar_localidad() {
+    let valido = true; 
+    var expregloc = /^[a-z]([a-z]|\s)+[a-z]$/i;
+    let localidad = document.formulario.localidad.value.trim();
+    if (expregloc.test(localidad)) {
+    document.formulario.error_localidad.value = "Error, la localidad está mal escrita";
+    document.formulario.error_localidad.style = "visibility: visible";
+    }
+    return valido;
+}
+//validar codigo postal
+function validar_codPostal() {
+    let valido = true; 
+    let codPostal = document.formulario.codPostal.value.trim();
+    let expregcodpos =/^(0?[1-9]|[1-4]\d|5[012])\d{3}$/i
+    if (!expregcodpos.test(codPostal)) { 
+        document.formulario.error_codpos.value= "Error, codigo postal no valido";
+        document.formulario.error_codpos.style= "visibility:visible"; 
+        valido = false;
+}
+return valido;
+}
